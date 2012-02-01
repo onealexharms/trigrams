27627d43109491fec62e911bdd217807e4caa015
Angela Harms <angela.harms@gmail.com>
2012-01-31 20:37:38 -0500

    fixed slowness. still a red test

--- lib/parseText.rb
+++ lib/parseText.rb
@@ -5,7 +5,7 @@ class ParseText
   end
   
   def source_words
-    @text.split
+    @source_words.nil? ? @source_words = @text.split : @source_words
   end
 
   def phrase_completing_words
@@ -13,7 +13,7 @@ class ParseText
     build_words = Array.new(size_of_array, [""])
     for index in (2..(source_words.length-1))
       word = source_words[index]
-      phrase = [source_words[index-2],source_words[index-1]]
+      phrase = [source_words[index-2], source_words[index-1]]
       index_in_pairs = pairs.find_index(phrase)
       build_words[index_in_pairs] = build_words[index_in_pairs] << [word]
     end 
@@ -22,9 +22,8 @@ class ParseText
 
   def pairs
     build_pairs = Array.new
-    source_words.each_with_index do
-      |first_word, index|
-      second_word = source_words[index + 1]||""
+    source_words.each_with_index do |first_word, index|
+      second_word = source_words[index + 1] || ""
       build_pairs << [first_word, second_word] 
     end
     build_pairs.uniq
